c69decf9503d19dd9bf02b8c7da41cf107c053c8
Ramón M. Gómez <rgomezlabra@gmail.com>
2017-10-25 20:59:44 +0200

    Ejercicio 2: crear primera entidad de la BD.

---
+++ src/Us/SymremedyBundle/Entity/User.php
@@ -0,0 +1,110 @@
+<?php
+// Class: User
+namespace Us\SymremedyBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+/**
+* @ORM\Entity(repositoryClass="Us\SymremedyBundle\Entity\Repository\UserRepository")
+* @ORM\Table(name="user")
+*/
+Class User {
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    /**
+     * @ORM\Column(name="username", type="string", length=20)
+     */
+    protected $username;
+    /**
+     * @ORM\Column(name="fullname", type="string", length=100)
+     */
+    protected $fullname;
+    /**
+     * @ORM\Column(name="password", type="string", length=56)
+     */
+    protected $password;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set fullname
+     *
+     * @param string $fullname
+     *
+     * @return User
+     */
+    public function setFullname($fullname)
+    {
+        $this->fullname = $fullname;
+
+        return $this;
+    }
+
+    /**
+     * Get fullname
+     *
+     * @return string
+     */
+    public function getFullname()
+    {
+        return $this->fullname;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+}

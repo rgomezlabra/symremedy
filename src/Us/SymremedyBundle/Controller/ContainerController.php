6a878081ce230acac9fc1830969e52b2b6af52bd
Ramón M. Gómez <rgomezlabra@gmail.com>
2017-11-10 18:45:07 +0100

    Mostrar espacio padre al crear contenedor y listar los espacios en forma de árbol.

--- src/Us/SymremedyBundle/Controller/ContainerController.php
+++ src/Us/SymremedyBundle/Controller/ContainerController.php
@@ -7,8 +7,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Us\SymremedyBundle\Entity\Container\Container;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -16,49 +18,51 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class ContainerController extends Controller
 {
     /**
-     * Route(
-     *     "/symremedy/container/create",
-     *     defaults={"id" = 0}
-     * )
-     * @Route(
-     *     "/symremedy/container/{id}/create",
-     *     requirements={"id" = "\d+"}
-     * )
+     * @Route("/symremedy/container/create", defaults={"id" = 0});
+     * @Route("/symremedy/container/{id}/create", requirements={"id" = "\d+"});
      */
-    public function createAction(int $id, Request $request)
+    public function createAction(int $id = 0, Request $request)
     {
         $response = null;
 	$container = new Container();
+        $parentName = '';
+        // Check parent.
+	$em = $this->getDoctrine()->getManager();
+        if ($id != 0)
+        {
+	    $parent = $em->getRepository(Container::class)->find($id);
+            if ($parent)
+            {
+                $parentName = $parent->getName();
+            } else
+            {
+                throw $this->createNotFoundException('No container found for parent id'.$id);
+            }
+        }
         // Create form.
 	$form = $this->createFormBuilder($container)
                      ->add('name', TextType::class, array('label' => 'Nombre: '))
                      ->add('description', TextType::class, array('label' => 'Descripción: '))
                      ->add('capacity', IntegerType::class, array('label' => 'Aforo: '))
+                     ->add('save', SubmitType::class, array('label' => 'Crear espacio'))
                      ->getForm();
         // Process request.
         $form->handleRequest($request);
         if ($form->isSubmitted() and $form->isValid())
         {
-	    $em = $this->getDoctrine()->getManager();
             if ($id != 0)
             {
-	        $parent = $em->getRepository(Container::class)->find($id);
-                if (!$parent)
-                {
-                    throw $this->createNotFoundException('No container found for parent id'.$id);
-                }
                 $container->setParent($parent);
             }
 	    $em->persist($container);
 	    $em->flush();
 	    $response = $this->redirect($this->generateUrl('us_symremedy_container_show',
                              array('id' => $container->getId())));
-	    //$response = $this->redirect($this->container->get('router')->generate('show',
         }
         if ($response == null)
         {
             $response = $this->render('UsSymremedyBundle:Container:new.html.twig',
-                             array('form' => $form->createView()));
+                             array('form' => $form->createView(), 'parent' => $parentName));
         }
 	return $response;
     }
@@ -102,9 +106,9 @@ class ContainerController extends Controller
     /**
      * @Route(
      *     "/symremedy/container/list/{order}",
-     *     defaults={"order": "ASC"},
-     *     requirements={"order": "ASC|DESC"}
-     * )
+     *     defaults={"order" = "ASC"},
+     *     requirements={"order" = "ASC|DESC"}
+     * );
      */
     public function listAction($order)
     {
@@ -113,7 +117,7 @@ class ContainerController extends Controller
             throw $this->createNotFoundException('List order must be ASC or DESC');
         }
         $containers = $this->getDoctrine()->getRepository(Container::class)->findBy(
-                      array(), array('name' => $order));
+                      array('parent' => null), array('name' => $order));
         return $this->render('UsSymremedyBundle:Container:list.html.twig',
                       array('containers' => $containers));
     }
